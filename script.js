e06a7e19fefe9037a42640904227bcb96da795db
GeorgiosKallarogiannis <115537120+GeorgiosKallarogiannis@users.noreply.github.com>
2025-12-15 21:06:54 +0100

    Refactore changeStatus function

--- script.js
+++ script.js
@@ -1,90 +1,73 @@
 "use strict";
 
-function Book(title, author, read = "unread") {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.read = read;
+function Book(title, author, pageNumber, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pageNumber = pageNumber;
+  this.read = read;
 }
+
+Book.prototype.changeStatus = function () {
+  this.read = !this.read;
+};
 
 function addBookToPage(array) {
-    const parentElement = document.querySelector("#bookSection");
+  const parentElement = document.querySelector("#bookSection");
 
-    while (parentElement.firstChild) {
-        parentElement.removeChild(parentElement.firstChild);
-    }
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.firstChild);
+  }
 
-    for (let i = 0; i <= array.length - 1; ++i) {
-        const newDiv = document.createElement("div");
-        const lineBreakOne = document.createElement("br");
-        const lineBreakTwo = document.createElement("br");
-        const remBtn = document.createElement("button");
-        const readBtn = document.createElement("button");
+  for (let i = 0; i <= array.length - 1; ++i) {
+    const newDiv = document.createElement("div");
+    const lineBreakOne = document.createElement("br");
+    const lineBreakTwo = document.createElement("br");
+    const remBtn = document.createElement("button");
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("change-state");
 
-        const bookInformation = document.createTextNode(`${array[i].title} by ${array[i].author}`);
-        const readStatus = document.createTextNode(`Status: ${array[i].read}`);
-        const remBtnContent = document.createTextNode("Remove Book");
-        const readBtnContent = document.createTextNode("Change read status")
+    const bookInformation = document.createTextNode(
+      `${array[i].title} by ${array[i].author}`
+    );
 
-        newDiv.appendChild(bookInformation);
-        newDiv.appendChild(lineBreakOne);
-        newDiv.appendChild(readStatus);
-        newDiv.appendChild(lineBreakTwo);
-        newDiv.appendChild(readBtn);
-        readBtn.appendChild(readBtnContent);
-        newDiv.appendChild(remBtn);
-        remBtn.appendChild(remBtnContent);
-       
-        newDiv.dataset.bookId = array[i].id;
-        
-        const parentDiv = document.querySelector("#bookSection");
-        parentDiv.appendChild(newDiv);
-    }
+    const read = array[i].read ? "Read" : "Unread";
+
+    const readStatus = document.createTextNode(`${read}`);
+    const remBtnContent = document.createTextNode("Remove Book");
+    const readBtnContent = document.createTextNode("Change read status");
+
+    newDiv.appendChild(bookInformation);
+    newDiv.appendChild(lineBreakOne);
+    newDiv.appendChild(readStatus);
+    newDiv.appendChild(lineBreakTwo);
+    newDiv.appendChild(readBtn);
+    readBtn.appendChild(readBtnContent);
+    newDiv.appendChild(remBtn);
+    remBtn.appendChild(remBtnContent);
+
+    newDiv.dataset.bookId = array[i].id;
+
+    const parentDiv = document.querySelector("#bookSection");
+    parentDiv.appendChild(newDiv);
+  }
 }
 
-function addBookToLibrary(title, author, read) {
-    const createdBook = new Book(title, author, read);
+function addBookToLibrary(title, author, pageNumber, read) {
+  const createdBook = new Book(title, author, pageNumber, read);
 
-    console.log(createdBook);
-    
+  console.log(createdBook);
 
-    myLibrary.push(createdBook);
+  myLibrary.push(createdBook);
 }
 
 function removeBook(array, dataAtt) {
-    const newLibrary = array.filter((obj) => obj.id != dataAtt);
+  const newLibrary = array.filter((obj) => obj.id != dataAtt);
 
-    return newLibrary;
-}
-
-function changeStatus(array, dataAtt) {
-
-    if (array.find((obj) => obj.id == dataAtt && obj.read == "read")) {
-        Object.assign(
-            array.find((obj) => obj.id == dataAtt),
-
-            {
-                read: "unread"
-            }
-        )
-    } else {
-        Object.assign(
-            array.find((obj) => obj.id == dataAtt),
-
-            {
-                read: "read"
-            }
-        )
-    }
-
-
-    addBookToPage(array)
+  return newLibrary;
 }
 
 let myLibrary = [];
-
-addBookToLibrary("Moby-Dick", "Herman Melville", "unread");
-addBookToLibrary("Homo Faber", "Max Frisch", "read");
 
 const dialog = document.querySelector("dialog");
 const showDialog = document.querySelector("button");
@@ -92,38 +75,52 @@ const closeDialog = document.querySelector("dialog button");
 const bookElement = document.querySelector("#bookSection");
 
 bookElement.addEventListener("click", (e) => {
-    
-    if (e.target.nodeName == "BUTTON" && e.target.innerText == "Remove Book") {
-        
-        myLibrary = removeBook(myLibrary, e.target.parentElement.dataset.bookId);
+  if (e.target.nodeName == "BUTTON" && e.target.innerText == "Remove Book") {
+    myLibrary = removeBook(myLibrary, e.target.parentElement.dataset.bookId);
 
+    addBookToPage(myLibrary);
+  }
+
+  if (
+    e.target.nodeName == "BUTTON" &&
+    e.target.innerText == "Change read status"
+  ) {
+    for (let book in myLibrary) {
+      if (myLibrary[book].id == e.target.parentElement.dataset.bookId) {
+        myLibrary[book].changeStatus();
         addBookToPage(myLibrary);
+      }
     }
-
-    if (e.target.nodeName == "BUTTON" && e.target.innerText == "Change read status") {
-        changeStatus(myLibrary, e.target.parentElement.dataset.bookId);
-    }
-})
-
+  }
+});
 
 const authorInput = document.querySelector("#author");
 const titleInput = document.querySelector("#title");
+const pageNumberInput = document.querySelector("#pageNumber");
+const readStatusInput = document.querySelector("#readStatus");
 
 showDialog.addEventListener("click", () => {
-    dialog.showModal();
-})
+  dialog.showModal();
+});
 
 closeDialog.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    const titleInputVal = titleInput.value;
-    const authorInputVal = authorInput.value;
-    
-    addBookToLibrary(titleInputVal, authorInputVal);
-    addBookToPage(myLibrary);
+  e.preventDefault();
 
-    dialog.close();
-})
+  const titleInputVal = titleInput.value;
+  const authorInputVal = authorInput.value;
+  const pageNumberInputVal = pageNumberInput.value;
+  const readStatusInputVal = readStatusInput.checked;
 
+  addBookToLibrary(
+    titleInputVal,
+    authorInputVal,
+    pageNumberInputVal,
+    readStatusInputVal
+  );
+
+  addBookToPage(myLibrary);
+
+  dialog.close();
+});
 
 addBookToPage(myLibrary);
